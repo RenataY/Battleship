a8f54dbedc763d1f610d1b250949d2c3f7e60181
Renata Yakobov <rinat.yuri@gmail.com>
2022-09-29 20:54:49 -0400

    Initial thoughts on needed steps

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1. Create grid html with javascript
+// 2. Create ships structure
+// 3. Logically place random ship on the board
+// 4. Place ships function - validate its on the board, doesn't clash with other ships
+// 5. Hit function
+// 6. Miss function
+// 7. Create other graphic parts of the game  
+// 8. Game over
+// 9. New game
