67bfb647d9044fa94b388e81ac3ccd38454dc04a
Renata Yakobov <rinat.yuri@gmail.com>
2022-09-30 15:08:02 -0400

    ships array in and images

---
+++ battleships.js
@@ -0,0 +1,53 @@
+export const shipsArray = [
+    {
+        id: 1,
+        name: "Aircraftcarrier",
+        size: 5,
+        image: ""
+
+    },
+    {
+        id: 2,
+        name: "Battleship",
+        size: 4,
+        image: ""
+
+    },
+    {
+        id: 3,
+        name: "Cruiser",
+        size: 3,
+        image: ""
+
+    },
+    {
+        id: 4,
+        name: "Submarine 1",
+        size: 2,
+        image: ""
+
+    },
+    {
+        id: 5,
+        name: "Submarine 2",
+        size: 2,
+        image: ""
+
+    },
+    {
+        id: 6,
+        name: "Patrol Boat 1",
+        size: 1,
+        image: ""
+
+    },
+    {
+        id: 7,
+        name: "Patrol Boat 2",
+        size: 1,
+        image: ""
+
+    }
+];
+
+export default shipsArray;
