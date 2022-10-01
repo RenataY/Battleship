c013108fbf5b2a75fc7b68eb6e63c3955aa206f0
Renata Yakobov <rinat.yuri@gmail.com>
2022-10-01 18:26:43 -0400

    tested image change to hit, crosshair cursor

--- battleships.js
+++ battleships.js
@@ -3,49 +3,61 @@ export const shipsArray = [
         id: 1,
         name: "Aircraftcarrier",
         size: 6,
-        image: ""
-
+        image: "./images/aircraft-carrier.png",
+        imageHit: "./images/aircraft-carrier-hit.png",
+        picClass: "aircraft"
     },
     {
         id: 2,
         name: "Battleship",
         size: 5,
-        image: ""
-
+        image: "./images/battleship.png",
+        imageHit: "./images/battleship-hit.png",
+        picClass: "battleship"    
     },
     {
         id: 3,
         name: "Cruiser",
         size: 3,
-        image: ""
+        image: "./images/cruiser.png",
+        imageHit: "./images/cruiser-hit.png",
+        picClass: "cruiser"
 
     },
     {
         id: 4,
         name: "Submarine 1",
         size: 3,
-        image: ""
+        image: "./images/submarine1.png",
+        imageHit: "./images/submarine1-hit.png",
+        picClass: "submarine1"
 
     },
     {
         id: 5,
         name: "Submarine 2",
         size: 3,
-        image: ""
+        image: "./images/submarine2.png",
+        imageHit: "./images/submarine2-hit.png",
+        picClass: "submarine2"
 
     },
     {
         id: 6,
         name: "Patrol Boat 1",
         size: 1,
-        image: ""
+        image: "./images/patrol-boat1.png",
+        imageHit: "./images/patrol-boat1-hit.png",
+        picClass: "patrol1"
 
     },
     {
         id: 7,
         name: "Patrol Boat 2",
         size: 1,
-        image: ""
+        image: "./images/patrol-boat2.png",
+        imageHit: "./images/patrol-boat2-hit.png",
+        picClass: "patrol2"       
 
     }
 ];
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// 1. Create grid html with javascript
-// 2. Create ships structure
+// 1. Done - Create grid html with javascript
+// 2. Done - Create ships structure
 // 3. Logically place random ship on the board
 // 4. Place ships function - validate its on the board, doesn't clash with other ships
 // 5. Hit function
 // 6. Miss function
-// 7. Create other graphic parts of the game  
+// 7. Done - Create other graphic parts of the game  
 // 8. Game over
-// 9. New game
+// 9. Done-  New game
 
 import shipsArray from "./battleships.js";
 let gameStarted = false;
@@ -26,7 +26,6 @@ const createBoardCell = (id) => {
 } 
 
 // create 10 x 10 board
-
 const createBoard = () => {
     for (let i=0; i<10; i++) {
         for (let j=0; j<10; j++) {
@@ -34,8 +33,22 @@ const createBoard = () => {
         }
     }
 }
+// function to create html for pictures of ships on the right side after game started
+const gameShipsPanel = () => {
+    for (let i=0; i<shipsArray.length; i++) {
+        shipsAndRules.innerHTML +=  `<img src=${shipsArray[i].image} alt="${shipsArray[i].name} image" class="ships-rules__ships__${shipsArray[i].picClass}"></img>`
+    }
+    return shipsAndRules.innerHTML;
+}
 
-playButton.addEventListener("click", (event) => {
+// function to replace a ship picture with hit ship picture
+const replaceToHitShipPic = (ship) => {
+    const shipPic = document.querySelector(".ships-rules__ships__" + ship );
+    console.log (shipPic);
+    shipPic.src = shipsArray[2].imageHit;
+}
+
+const prepareNewGame = () => {
     gameStarted = true;
     console.log (gameStarted);
     container.innerHTML="";
@@ -43,26 +56,26 @@ playButton.addEventListener("click", (event) => {
     // add event listener for all buttons inside the function
     createBoard();
     shipsAndRules.innerHTML = "";
-    // create array of objects for pictures and classes and refactor below to function
     shipsAndRules.innerHTML = 
                     `<div class="ships-rules__ships">
-                        <img src="./images/aircraft-carrier.png" alt="aircraft carrier image" class="ships-rules__ships__aircraft">
-                        <img src="./images/battleship.png" alt="battleship image" class="ships-rules__ships__battleship">
-                        <img src="./images/cruiser.png" alt="cruiser image" class="ships-rules__ships__cruiser">
-                        <img src="./images/submarine1.png" alt="submarine image" class="ships-rules__ships__submarine1">
-                        <img src="./images/submarine2.png" alt="submarine image" class="ships-rules__ships__submarine2">
-                        <img src="./images/patrol-boat1.png" alt="patrolboat 1 image" class="ships-rules__ships__patrol1"> 
-                        <img src="./images/patrol-boat2.png" alt="patrolboat 2 image" class="ships-rules__ships__patrol2"> 
+                        ${gameShipsPanel()}                        
                         <button type="button" class="ships-rules__new-game">New Game</button>
-                    </div>`
-     // get one button and try to click, concerned as it is transparent
+                    </div>`     
+    
+    const newGameButton = document.querySelector(".ships-rules__new-game");
+    newGameButton.addEventListener ("click", (event) => {
+        console.log("New Game button clicked")
+        prepareNewGame();
+    })
+    // get one button and try to click
      const btn1 = document.getElementById("01");
      btn1.addEventListener ("click", (event) => {
          btn1.style.backgroundImage = "url(./images/explosion1.gif)";
          btn1.style.backgroundSize = "cover";
+         replaceToHitShipPic ("cruiser");
      })
 
-     // get another button and try to click, concerned as it is transparent
+     // get another button and try to click
      const btn2 = document.getElementById("02");
      btn2.addEventListener ("click", (event) => {
          console.log("button 2 clicked");
@@ -70,7 +83,7 @@ playButton.addEventListener("click", (event) => {
          btn2.style.backgroundSize = "cover";
      })
 
-     // get another button and try to click, concerned as it is transparent
+     // get another button and try to click
      const btn3 = document.getElementById("03");
      btn3.addEventListener ("click", (event) => {
          console.log("button 3 clicked");
@@ -78,5 +91,12 @@ playButton.addEventListener("click", (event) => {
          btn3.style.backgroundSize = "cover";
      })
              
+}
+
+// start the game, main function
+playButton.addEventListener("click", (event) => {
+    prepareNewGame();
 })
+
+
 
